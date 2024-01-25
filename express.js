829da9b007b0fa519e668db4b4b04e1166c1868b
AddarshKumar123 <addarshkumar2005@gmail.com>
2024-01-25 11:53:50 +0530

    server

---
+++ express.js
@@ -0,0 +1,10 @@
+const express=require("express");
+const app=express();
+
+app.get("/ping",(req,res)=>{
+    res.send("pong")
+})
+
+app.listen(4000,()=>{
+    console.log("listnening on port 4000");
+})
