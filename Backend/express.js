db569db05e1b20e247b5f58672629eebf04dbc14
AddarshKumar123 <addarshkumar2005@gmail.com>
2024-02-04 12:08:01 +0530

    readme file updated

--- Backend/express.js
+++ Backend/express.js
@@ -1,8 +1,10 @@
 const express=require("express");
 const mongoose=require("mongoose");
 const config=require("./Database/db");
+const cors=require("cors");
 const app=express();
 app.use(express.json())
+app.use(cors())
 
 app.get("/ping",(req,res)=>{ 
     res.send("pong")
